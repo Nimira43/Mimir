6ddfe9685b961b08a96170625f546d5bcb3c310b
Nimira43 <n.rabone72@aol.com>
2025-08-30 09:32:56 +0100

    Set up app and middleware.

--- mimir-api/src/server.ts
+++ mimir-api/src/server.ts
@@ -2,3 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
