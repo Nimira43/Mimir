3e4469658e1d4f8fa76725715a79990db4c84cdd
Nimira43 <n.rabone72@aol.com>
2025-09-11 03:13:49 +0100

    Finished setting up server

--- mimir-api/src/server.ts
+++ mimir-api/src/server.ts
@@ -10,3 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
+
